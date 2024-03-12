93dd0eb76d17d8f3544deb334d59301922f7bd05
minimario47 <126113414+minimario47@users.noreply.github.com>
2024-03-12 21:06:09 +0100

    Polygon data (Verkstad/ Uppställningsplatser)

--- js/qgis2web_expressions.js
+++ js/qgis2web_expressions.js
@@ -754,17 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_label_VTGteborg_2_eval_expression(context) {
-    // SparbetBes || ', ' || _length || 'm'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return (((feature.properties['SparbetBes']  + ', ') + feature.properties['_length'] ) + 'm');
-    } else {
-        return (((feature['SparbetBes']  + ', ') + feature['_length'] ) + 'm');
-    }
-}
